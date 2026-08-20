f5fc9900d2805f3ceb3b55da4a45cdf9cdfc77c8
arisvia <160387885+arisvia@users.noreply.github.com>
2026-08-20 16:48:59 +0800

    fix(windows): correct variable-parameter forwarding macros in dwmapi.cpp proxy

--- src/Platform/Windows/Proxy/dwmapi.cpp
+++ src/Platform/Windows/Proxy/dwmapi.cpp
@@ -39,48 +39,98 @@ void LoadOmniSteamCore() {
 
 extern "C" {
 
-#define FORWARD_DWM(name, retType, ...)                                                                                \
-    __declspec(dllexport) retType WINAPI name(__VA_ARGS__) {                                                           \
+#define FORWARD_DWM_0(name, retType)                                                                                   \
+    __declspec(dllexport) retType WINAPI name() {                                                                      \
         LoadOmniSteamCore();                                                                                           \
-        typedef retType(WINAPI * tFunc)(__VA_ARGS__);                                                                  \
+        typedef retType(WINAPI * tFunc)();                                                                             \
         static tFunc pFunc = (tFunc)GetRealFunc(#name);                                                                \
         if (pFunc)                                                                                                     \
-            return pFunc(__VA_ARGS__);                                                                                 \
+            return pFunc();                                                                                            \
         return (retType)0;                                                                                             \
     }
 
-FORWARD_DWM(DwmAttachMilContent, HRESULT, HWND hwnd)
-FORWARD_DWM(DwmDefWindowProc, BOOL, HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam, LRESULT* plResult)
-FORWARD_DWM(DwmDetachMilContent, HRESULT, HWND hwnd)
-FORWARD_DWM(DwmEnableBlurBehindWindow, HRESULT, HWND hWnd, const void* pBlurBehind)
-FORWARD_DWM(DwmEnableComposition, HRESULT, UINT uCompositionAction)
-FORWARD_DWM(DwmEnableMMCSS, HRESULT, BOOL fEnableMMCSS)
-FORWARD_DWM(DwmExtendFrameIntoClientArea, HRESULT, HWND hWnd, const void* pMarInset)
-FORWARD_DWM(DwmFlush, HRESULT)
-FORWARD_DWM(DwmGetColorizationColor, HRESULT, DWORD* pcrColorization, BOOL* pfOpaqueBlend)
-FORWARD_DWM(DwmGetCompositionTimingInfo, HRESULT, HWND hwnd, void* pTimingInfo)
-FORWARD_DWM(DwmGetGraphicsStreamClient, HRESULT, UINT uIndex, void* pClient)
-FORWARD_DWM(DwmGetGraphicsStreamTransformHint, HRESULT, UINT uIndex, void* pTransform)
-FORWARD_DWM(DwmGetTransportAttributes, HRESULT, BOOL* pfIsRemoting, BOOL* pfIsConnected, DWORD* pDwGeneration)
-FORWARD_DWM(DwmGetUnmetTabRequirements, HRESULT, HWND hwnd, void* pRequirements)
-FORWARD_DWM(DwmGetWindowAttribute, HRESULT, HWND hwnd, DWORD dwAttribute, PVOID pvAttribute, DWORD cbAttribute)
-FORWARD_DWM(DwmInvalidateIconicBitmaps, HRESULT, HWND hwnd)
-FORWARD_DWM(DwmIsCompositionEnabled, HRESULT, BOOL* pfEnabled)
-FORWARD_DWM(DwmModifyPreviousDxFrameDuration, HRESULT, HWND hwnd, INT cRefreshes, BOOL fRelative)
-FORWARD_DWM(DwmQueryThumbnailSourceSize, HRESULT, void* hThumbnail, void* pSize)
-FORWARD_DWM(DwmRegisterThumbnail, HRESULT, HWND hwndDestination, HWND hwndSource, void* phThumbnailId)
-FORWARD_DWM(DwmRenderGesture, HRESULT, void* gt, UINT cPoints, void* pPoints, void* pPointsUpdated)
-FORWARD_DWM(DwmSetColorizationParameters, HRESULT, void* pParameters, UINT uUnknown)
-FORWARD_DWM(DwmSetDxFrameDuration, HRESULT, HWND hwnd, INT cRefreshes)
-FORWARD_DWM(DwmSetIconicLivePreviewBitmap, HRESULT, HWND hwnd, void* hbmp, void* pptClient, DWORD dwSITFlags)
-FORWARD_DWM(DwmSetIconicThumbnail, HRESULT, HWND hwnd, void* hbmp, DWORD dwSITFlags)
-FORWARD_DWM(DwmSetPresentParameters, HRESULT, HWND hwnd, void* pPresentParams)
-FORWARD_DWM(DwmSetWindowAttribute, HRESULT, HWND hwnd, DWORD dwAttribute, LPCVOID pvAttribute, DWORD cbAttribute)
-FORWARD_DWM(DwmShowContact, HRESULT, DWORD dwPointerId, void* eShowContact)
-FORWARD_DWM(DwmTetherContact, HRESULT, DWORD dwPointerId, BOOL fEnable, void* pptTether)
-FORWARD_DWM(DwmTransitionOwnedWindow, HRESULT, HWND hwnd, void* target)
-FORWARD_DWM(DwmUnregisterThumbnail, HRESULT, void* hThumbnailId)
-FORWARD_DWM(DwmUpdateThumbnailProperties, HRESULT, void* hThumbnailId, const void* ptnProperties)
+#define FORWARD_DWM_1(name, retType, T1, a1)                                                                           \
+    __declspec(dllexport) retType WINAPI name(T1 a1) {                                                                 \
+        LoadOmniSteamCore();                                                                                           \
+        typedef retType(WINAPI * tFunc)(T1);                                                                           \
+        static tFunc pFunc = (tFunc)GetRealFunc(#name);                                                                \
+        if (pFunc)                                                                                                     \
+            return pFunc(a1);                                                                                          \
+        return (retType)0;                                                                                             \
+    }
+
+#define FORWARD_DWM_2(name, retType, T1, a1, T2, a2)                                                                   \
+    __declspec(dllexport) retType WINAPI name(T1 a1, T2 a2) {                                                          \
+        LoadOmniSteamCore();                                                                                           \
+        typedef retType(WINAPI * tFunc)(T1, T2);                                                                       \
+        static tFunc pFunc = (tFunc)GetRealFunc(#name);                                                                \
+        if (pFunc)                                                                                                     \
+            return pFunc(a1, a2);                                                                                      \
+        return (retType)0;                                                                                             \
+    }
+
+#define FORWARD_DWM_3(name, retType, T1, a1, T2, a2, T3, a3)                                                           \
+    __declspec(dllexport) retType WINAPI name(T1 a1, T2 a2, T3 a3) {                                                   \
+        LoadOmniSteamCore();                                                                                           \
+        typedef retType(WINAPI * tFunc)(T1, T2, T3);                                                                   \
+        static tFunc pFunc = (tFunc)GetRealFunc(#name);                                                                \
+        if (pFunc)                                                                                                     \
+            return pFunc(a1, a2, a3);                                                                                  \
+        return (retType)0;                                                                                             \
+    }
+
+#define FORWARD_DWM_4(name, retType, T1, a1, T2, a2, T3, a3, T4, a4)                                                   \
+    __declspec(dllexport) retType WINAPI name(T1 a1, T2 a2, T3 a3, T4 a4) {                                            \
+        LoadOmniSteamCore();                                                                                           \
+        typedef retType(WINAPI * tFunc)(T1, T2, T3, T4);                                                               \
+        static tFunc pFunc = (tFunc)GetRealFunc(#name);                                                                \
+        if (pFunc)                                                                                                     \
+            return pFunc(a1, a2, a3, a4);                                                                              \
+        return (retType)0;                                                                                             \
+    }
+
+#define FORWARD_DWM_5(name, retType, T1, a1, T2, a2, T3, a3, T4, a4, T5, a5)                                           \
+    __declspec(dllexport) retType WINAPI name(T1 a1, T2 a2, T3 a3, T4 a4, T5 a5) {                                     \
+        LoadOmniSteamCore();                                                                                           \
+        typedef retType(WINAPI * tFunc)(T1, T2, T3, T4, T5);                                                           \
+        static tFunc pFunc = (tFunc)GetRealFunc(#name);                                                                \
+        if (pFunc)                                                                                                     \
+            return pFunc(a1, a2, a3, a4, a5);                                                                          \
+        return (retType)0;                                                                                             \
+    }
+
+FORWARD_DWM_1(DwmAttachMilContent, HRESULT, HWND, hwnd)
+FORWARD_DWM_5(DwmDefWindowProc, BOOL, HWND, hWnd, UINT, msg, WPARAM, wParam, LPARAM, lParam, LRESULT*, plResult)
+FORWARD_DWM_1(DwmDetachMilContent, HRESULT, HWND, hwnd)
+FORWARD_DWM_2(DwmEnableBlurBehindWindow, HRESULT, HWND, hWnd, const void*, pBlurBehind)
+FORWARD_DWM_1(DwmEnableComposition, HRESULT, UINT, uCompositionAction)
+FORWARD_DWM_1(DwmEnableMMCSS, HRESULT, BOOL, fEnableMMCSS)
+FORWARD_DWM_2(DwmExtendFrameIntoClientArea, HRESULT, HWND, hWnd, const void*, pMarInset)
+FORWARD_DWM_0(DwmFlush, HRESULT)
+FORWARD_DWM_2(DwmGetColorizationColor, HRESULT, DWORD*, pcrColorization, BOOL*, pfOpaqueBlend)
+FORWARD_DWM_2(DwmGetCompositionTimingInfo, HRESULT, HWND, hwnd, void*, pTimingInfo)
+FORWARD_DWM_2(DwmGetGraphicsStreamClient, HRESULT, UINT, uIndex, void*, pClient)
+FORWARD_DWM_2(DwmGetGraphicsStreamTransformHint, HRESULT, UINT, uIndex, void*, pTransform)
+FORWARD_DWM_3(DwmGetTransportAttributes, HRESULT, BOOL*, pfIsRemoting, BOOL*, pfIsConnected, DWORD*, pDwGeneration)
+FORWARD_DWM_2(DwmGetUnmetTabRequirements, HRESULT, HWND, hwnd, void*, pRequirements)
+FORWARD_DWM_4(DwmGetWindowAttribute, HRESULT, HWND, hwnd, DWORD, dwAttribute, PVOID, pvAttribute, DWORD, cbAttribute)
+FORWARD_DWM_1(DwmInvalidateIconicBitmaps, HRESULT, HWND, hwnd)
+FORWARD_DWM_1(DwmIsCompositionEnabled, HRESULT, BOOL*, pfEnabled)
+FORWARD_DWM_3(DwmModifyPreviousDxFrameDuration, HRESULT, HWND, hwnd, INT, cRefreshes, BOOL, fRelative)
+FORWARD_DWM_2(DwmQueryThumbnailSourceSize, HRESULT, void*, hThumbnail, void*, pSize)
+FORWARD_DWM_3(DwmRegisterThumbnail, HRESULT, HWND, hwndDestination, HWND, hwndSource, void*, phThumbnailId)
+FORWARD_DWM_4(DwmRenderGesture, HRESULT, void*, gt, UINT, cPoints, void*, pPoints, void*, pPointsUpdated)
+FORWARD_DWM_2(DwmSetColorizationParameters, HRESULT, void*, pParameters, UINT, uUnknown)
+FORWARD_DWM_2(DwmSetDxFrameDuration, HRESULT, HWND, hwnd, INT, cRefreshes)
+FORWARD_DWM_4(DwmSetIconicLivePreviewBitmap, HRESULT, HWND, hwnd, void*, hbmp, void*, pptClient, DWORD, dwSITFlags)
+FORWARD_DWM_3(DwmSetIconicThumbnail, HRESULT, HWND, hwnd, void*, hbmp, DWORD, dwSITFlags)
+FORWARD_DWM_2(DwmSetPresentParameters, HRESULT, HWND, hwnd, void*, pPresentParams)
+FORWARD_DWM_4(DwmSetWindowAttribute, HRESULT, HWND, hwnd, DWORD, dwAttribute, LPCVOID, pvAttribute, DWORD, cbAttribute)
+FORWARD_DWM_2(DwmShowContact, HRESULT, DWORD, dwPointerId, void*, eShowContact)
+FORWARD_DWM_3(DwmTetherContact, HRESULT, DWORD, dwPointerId, BOOL, fEnable, void*, pptTether)
+FORWARD_DWM_2(DwmTransitionOwnedWindow, HRESULT, HWND, hwnd, void*, target)
+FORWARD_DWM_1(DwmUnregisterThumbnail, HRESULT, void*, hThumbnailId)
+FORWARD_DWM_2(DwmUpdateThumbnailProperties, HRESULT, void*, hThumbnailId, const void*, ptnProperties)
 
 } // extern "C"
 
